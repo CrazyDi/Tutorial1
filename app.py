b241ec334a1d25abba06985728eea78fa695f4f5
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2019-11-16 15:19:26 +0300

    first step

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/add_two_nums', methods=["POST"])
+def add_two_nums():
+    data_dict = request.get_json()
+    x = data_dict["x"]
+    y = data_dict["y"]
+    z = x + y
+
+    res = {"z": z}
+    return jsonify(res), 200
 
 
 if __name__ == '__main__':
